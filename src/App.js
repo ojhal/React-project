2825c5ab7f8e69e245b060bfb114ff801530601a
Ojhal Patwa <patkarojhal26@gmail.com>
2021-05-05 22:20:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+  handleClick = () => {
+
+    console.log('clicked')
+  }
+
+
+
   render() {
+    <div>
+      <h1>hello world</h1>
+    </div>
     return (
       <div>
-        <h1 style={{ color: "red" }}>Hello World</h1>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoxqcJ9gupPODmqelu9fEvNrzc1GAt7yAR3i4AMt3Wh9vgvyEiKRQ0r24LeSb4Kfk4g34&usqp=CAU" />
+
+        <button onClick={this.handleClick}>Click me</button>
 
       </div>
     )
