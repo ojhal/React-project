ec23aa0782ca366bc304fdb96d7a883e30b0cb10
Ojhal Patwa <patkarojhal26@gmail.com>
2021-05-15 23:20:48 +0530

    use componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
 import React from "react";
-
-const App = () => {
-  return <h1> My name is Ojhal Patwa</h1>
-
-};
+import './App.css';
 
 
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    }
+    // console.log("constructor called")
+
+  }
+
+  // handleClick = () => {
+
+  //   console.log('clicked')
+  // }
+  componentDidMount() {
+    fetch('https://reqres.in/api/users?page=2').then((response) => {
+      response.json().then((result) => {
+        console.log(result)
+      })
+    })
+    // console.log("mounted")
+  }
 
 
+
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+
+        {/* 
+        <button onClick={this.handleClick}>Click me</button> */}
+
+      </div>
+    )
+  }
+}
 export default App;
-
-// export default class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     console.log("constructor called")
-
-//   }
-//   handleClick = () => {
-
-//     console.log('clicked')
-//   }
-//   componentDidMount() {
-//     console.log("mounted")
-//   }
-
-
-
-//   render() {
-//     console.log("render called")
-
-//     return (
-//       <div>
-//         <h1>Hello world</h1>
-
-
-//         <button onClick={this.handleClick}>Click me</button>
-
-//       </div>
-//     )
-//   }
-// }
-
